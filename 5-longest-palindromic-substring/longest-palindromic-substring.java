455c909574a4fcab1a59421ad499ed05188685db
Love Kumar Chaudhary <lovechaudhary1090@gmail.com>
2026-05-16 19:13:09 +0530

    Time: 558 ms (14.10%) | Memory: 47 MB (17.94%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length()<=1) return s;
+
+        int max=1;
+        String maxLen = s.substring(0,1);
+
+        for(int i=0;i<s.length();i++){
+            for(int j=i+max;j<=s.length();j++){
+                if(j-i>max && check(s.substring(i,j))){
+                    max=j-i;
+                    maxLen=s.substring(i,j);
+                }
+            }
+        }
+        return maxLen;
+    }
+    private static boolean check(String s){
+        int l=0;
+        int r=s.length()-1;
+
+        while(l<r){
+            if(s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
