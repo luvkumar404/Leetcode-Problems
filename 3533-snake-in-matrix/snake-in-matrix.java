9acf8adfb6ec0d252edf4819d1f3595de01ab938
Love Kumar Chaudhary <lovechaudhary1090@gmail.com>
2026-07-18 21:39:34 +0530

    Time: 2 ms (94.69%) | Memory: 46.4 MB (53.90%) - LeetSync

---
+++ 3533-snake-in-matrix/snake-in-matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int r=0, c=0;
+        for(String cmd:commands){
+            if(cmd.equals("UP")) r--;
+            else if(cmd.equals("DOWN")) r++;
+            else if(cmd.equals("LEFT")) c--;
+            else c++;
+        }
+        return r*n+c;
+    }
+}
